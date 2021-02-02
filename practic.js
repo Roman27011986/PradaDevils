93a6c6b2113dd6f2fbcb9b67f47e91f22b36a99c
Roman27011986 <didorsn@gmil.com>
2021-02-02 15:05:01 +0200

    й

--- practic.js
+++ practic.js
@@ -1,25 +1,73 @@
-const Hero = function(name, xp) {
-  this.name = name;
-  this.xp = xp;
-};
-// console.dir(Hero)
-Hero.prototype.gainXp = function(amount) {
-  console.log(`${this.name} gained ${amount} experience points`);
-  this.xp += amount;
-};
+// const Hero = function(name, xp) {
+//   this.name = name;
+//   this.xp = xp;
+// };
+// // console.dir(Hero)
+// Hero.prototype.gainXp = function(amount) {
+//   console.log(`${this.name} gained ${amount} experience points`);
+//   this.xp += amount;
+// };
 
-const Warrior = function(name, xp, weapon) {
-  Hero.call(this, name, xp);
+// const Warrior = function(name, xp, weapon) {
+//   Hero.call(this, name, xp);
 
-  this.weapon = weapon;
-};
+//   this.weapon = weapon;
+// };
 
-Warrior.prototype = Object.create(Hero.prototype);
-Warrior.prototype.constructor = Warrior;
+// Warrior.prototype = Object.create(Hero.prototype);
+// Warrior.prototype.constructor = Warrior;
 
-Warrior.prototype.attack = function() {
-  console.log(`${this.name} attacks with ${this.weapon}`);
-};
+// Warrior.prototype.attack = function() {
+//   console.log(`${this.name} attacks with ${this.weapon}`);
+// };
 
-const poly = new Warrior('Poly', 200, 'sword');
-console.dir(Warrior)
+// const poly = new Warrior('Poly', 200, 'sword');
+// console.dir(Warrior)
+
+
+
+
+
+
+
+
+// const players = [
+//   {id:'player - 1',name: 'Mango',timePlayed: 310,points: 54,online: false},
+//   {id:'player - 2',name: 'Poly',timePlayed: 210,points: 94,online: true},
+//   {id:'player - 3',name: 'Ajax',timePlayed: 110,points: 43,online: false},
+//   {id:'player - 4',name: 'Kiwi',timePlayed: 510,points: 15,online: true}
+// ]
+
+
+// ******* Из массива обьектов вернули НОВЫЙ массив обьектов и изменили свойство points ************
+
+
+// const updatePlayers = players.map(player => {
+
+//   return {
+//     ...player,
+//      points: player.points + player.points 
+//   };
+// });
+
+// ********Обновили свойство одного конкретного обьекта и вернули массив всех обьектов*************
+
+// const updatePlayers = players.map(player => {
+//   if (player.id === 'player - 2')
+//   return {
+//     ...player,
+//      points:  player.points + 50
+//     };
+//   return player
+// });
+
+// ********Палиндром*******
+
+function qwe(str) {
+ str = str.toLowerCase()
+ let str1 = str.split('').reverse().join('')
+  return str1 === str
+}
+
+
+console.log(qwe('AbbA'))
